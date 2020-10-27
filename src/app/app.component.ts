c5a96e73a3d4cd5e944d844e9c0a48233d2dca8e
unknown <ahmeddahi700@gmail.com>
2020-10-27 23:29:24 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CoronaService} from './services/corona.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Covid19Tracker';
+  countries :any;
+  country:any;
+  confirmed:number;
+  recovered:number;
+  deaths:number;
+  constructor(private corona:CoronaService){}
+  ngOnInit(){
+    this.corona.getCountries().subscribe((data)=>{
+    this.countries=data;
+    })
+   
+  } 
+  getCoronaData(){
+      this.corona.getCoronaRealTimeData(this.country).subscribe((data)=>{
+        var index=data.length-1;
+        this.confirmed=data[index].Confirmed;
+        this.recovered=data[index].Recovered;
+        this.deaths=data[index].Deaths;
+
+      })
+    }
+    getCountry(country:any){
+      this.country=country;
+    }
 }
